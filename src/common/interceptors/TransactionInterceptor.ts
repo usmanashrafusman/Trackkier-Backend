ad246840783cd5ca5e63c41aa5ca39d2aa456fbb
Muhammad Usman <usman.ashraf033@gmail.com>
2024-01-23 15:32:23 +0500

    initialized entity manager into transaction interceptor

--- src/common/interceptors/TransactionInterceptor.ts
+++ src/common/interceptors/TransactionInterceptor.ts
@@ -1,29 +1,35 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
-import { DataSource } from "typeorm";
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  Scope,
+} from '@nestjs/common';
 import { Observable, catchError, concatMap, finalize } from 'rxjs';
-import EntityManager from "src/common/entity-manager";
+import EntityManagerService from 'src/entity-manager/entity-manager.service';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class TransactionInterceptor implements NestInterceptor {
+  constructor(private readonly entityManager: EntityManagerService) {}
+  async intercept(
+    _: ExecutionContext,
+    next: CallHandler<any>,
+  ): Promise<Observable<any>> {
+    await this.entityManager.initialize();
+    const queryRunner = this.entityManager.queryRunner;
 
-    constructor(private dataSource: DataSource) { }
-    async intercept(_: ExecutionContext, next: CallHandler<any>): Promise<Observable<any>> {
-        //setting the datasource to entity manager to set query manager and start transaction
-        await EntityManager.setDataSource(this.dataSource)
-        const queryRunner = EntityManager.queryRunner;
-
-        return next.handle().pipe(
-            concatMap(async (data) => {
-                await queryRunner.commitTransaction();
-                return data;
-            }),
-            catchError(async (e) => {
-                await queryRunner.rollbackTransaction();
-                throw e;
-            }),
-            finalize(async () => {
-                await queryRunner.release();
-            }),
-        );
-    }
+    return next.handle().pipe(
+      concatMap(async (data) => {
+        await queryRunner.commitTransaction();
+        return data;
+      }),
+      catchError(async (e) => {
+        await queryRunner.rollbackTransaction();
+        throw e;
+      }),
+      finalize(async () => {
+        await queryRunner.release();
+      }),
+    );
+  }
 }
