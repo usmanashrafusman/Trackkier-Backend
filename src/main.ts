0a9ac9d51f146d9afe7ba15f23a7539eda4018cc
Muhammad Usman <usman.ashraf033@gmail.com>
2024-01-29 15:37:24 +0500

    added hemlet

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+
+import { AppModule } from 'src/app.module';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
-  const config = app.get(ConfigService)
-  const PORT = config.getOrThrow("PORT");
+  const config = app.get(ConfigService);
+  const PORT = config.getOrThrow('PORT');
+
+  app.use(helmet());
+  app.use(cookieParser());
 
   app.enableCors();
-  app.use(cookieParser());
-  app.setGlobalPrefix("api/v1");
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    forbidNonWhitelisted: true,
-  }));
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(PORT);
-})()
+})();
